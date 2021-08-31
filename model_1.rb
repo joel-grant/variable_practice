8311f71c0a6672b66a1188725d08445f9159067b
Joel Grant <joelmgrant@me.com>
2021-08-30 21:43:03 -0600

    Add hashes

--- model_1.rb
+++ model_1.rb
@@ -1,12 +1,20 @@
+# strings
 name = "Rocky"
 creature = "Dog"
 
+# integer and float
 age = 2
 weight = 74.4 #weight in pounds
 
+# boolean
 is_cute = true
 causes_trouble = true
 helps_with_chores = false
 
+# arrays
 items_chewed = ["Playstation VR", "Headphones", "Countless Socks"]
 favorite_toys = ["Ball", "Bone", "Tire"]
+
+# hash
+commands = { "Sit" => 1, "Down" => 2, "Rollover" => 3, "Stay" => 4 }
+words_he_understands = { "Breakfast" => 1, "Walk" => 2, "Treat" => 3 }
