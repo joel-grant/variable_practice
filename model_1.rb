a52d121562302c6e3a1766ba4d7ad95b386de89d
Joel Grant <joelmgrant@me.com>
2021-08-30 20:35:15 -0600

    Add arrays

--- model_1.rb
+++ model_1.rb
@@ -7,3 +7,6 @@ weight = 74.4 #weight in pounds
 is_cute = true
 causes_trouble = true
 helps_with_chores = false
+
+items_chewed = ["Playstation VR", "Headphones", "Countless Socks"]
+favorite_toys = ["Ball", "Bone", "Tire"]
